e8979b97fb3674499a50424f568d4d13b3634f24
levip <levipietrosousa@gmail.com>
2024-11-24 11:08:44 -0300

    finalização do objeto book

--- src/main/java/br/com/booknest/model/Book.java
+++ src/main/java/br/com/booknest/model/Book.java
@@ -5,11 +5,35 @@ public class Book {
     String corCapaLivro = "Red";
     String tituloCapa = "O Principe";
     String authorLivro = "Maquiavel";
+    int numeroPagina = 92;
+    int lancamentoAno = 1485;
+    String editoraLivro = "Editora123";
+    String idiomaLivro = "Português";
+    double valorLivro = 40;
+
+    public Book(String corCapaLivro, String tituloCapa, String authorLivro, int numeroPagina, int lancamentoAno, String editoraLivro, String idiomaLivro, int valorLivro) {
+
+        this.corCapaLivro = corCapaLivro;
+        this.authorLivro = authorLivro;
+        this.editoraLivro = editoraLivro;
+        this.tituloCapa = tituloCapa;
+        this.numeroPagina = numeroPagina;
+        this.lancamentoAno = lancamentoAno;
+        this.valorLivro = valorLivro;
+        this.idiomaLivro = idiomaLivro;
+
+
+    }
+
+    public Book() {
+
+    }
 
     public void lerLivro() {
 
         System.out.println("Lendo Livro...");
     }
+
 
     public String getCorCapaLivro() {
         return corCapaLivro;
@@ -17,5 +41,61 @@ public class Book {
 
     public void setCorCapaLivro(String corCapaLivro) {
         this.corCapaLivro = corCapaLivro;
+    }
+
+    public String getTituloCapa() {
+        return tituloCapa;
+    }
+
+    public void setTituloCapa(String tituloCapa) {
+        this.tituloCapa = tituloCapa;
+    }
+
+    public String getAuthorLivro() {
+        return authorLivro;
+    }
+
+    public void setAuthorLivro(String authorLivro) {
+        this.authorLivro = authorLivro;
+    }
+
+    public int getNumeroPagina() {
+        return numeroPagina;
+    }
+
+    public void setNumeroPagina(int numeroPagina) {
+        this.numeroPagina = numeroPagina;
+    }
+
+    public int getLancamentoAno() {
+        return lancamentoAno;
+    }
+
+    public void setLancamentoAno(int lancamentoAno) {
+        this.lancamentoAno = lancamentoAno;
+    }
+
+    public String getEditoraLivro() {
+        return editoraLivro;
+    }
+
+    public void setEditoraLivro(String editoraLivro) {
+        this.editoraLivro = editoraLivro;
+    }
+
+    public String getIdiomaLivro() {
+        return idiomaLivro;
+    }
+
+    public void setIdiomaLivro(String idiomaLivro) {
+        this.idiomaLivro = idiomaLivro;
+    }
+
+    public double getValorLivro() {
+        return valorLivro;
+    }
+
+    public void setValorLivro(double valorLivro) {
+        this.valorLivro = valorLivro;
     }
 }
